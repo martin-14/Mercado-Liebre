e5112172cc11421f10f4f65701d6619fa487173b
martin-14 <martin14.09.2002@gmail.com>
2021-06-05 09:47:40 -0300

    Maqueta Web

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require('path');
 const port = 3030;
 
-
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, '/views/home.html'))
+})
 
 app.listen(port, () => {
     console.log('Servidor corriendo en el puerto' + ' ' + port);
 })
+app.use(express.static('public'));
