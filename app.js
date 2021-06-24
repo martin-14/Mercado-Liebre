a81e533dd96e47e9ad2e7e775dc1963ef6534d80
martin-14 <martin14.09.2002@gmail.com>
2021-06-24 08:00:48 -0300

    TP 2 terminado

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const port = 3030;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, '/views/home.html'))
 })
@@ -10,4 +12,3 @@ res.sendFile(path.join(__dirname, '/views/home.html'))
 app.listen(port, () => {
     console.log('Servidor corriendo en el puerto' + ' ' + port);
 })
-app.use(express.static('public'));
